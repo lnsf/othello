fad920e1ee6b0b78eaaa49233ee746438c0a36ae
lnsf <lnsf03@gmail.com>
2019-09-08 21:10:26 +0900

    :tada: Make stones REVERSIBLE!

--- src/game.js
+++ src/game.js
@@ -17,11 +17,60 @@ class Game extends React.Component {
   }
 
   handleClick(i) {
+    const reversible = this.findReversible(i);
+    
+    if(reversible.length === 0){
+      return;
+    }
+    
     let next = this.state;
     next.values[i] = this.state.blackIsNext ? 1 : -1;
-    next.blackIsNext = !this.state.blackIsNext;
 
+    reversible.forEach((r) => {
+      next.values[r] = this.state.blackIsNext ? 1 : -1;
+    });
+    
+    next.blackIsNext = !this.state.blackIsNext;
     this.setState(next);
+  }
+
+  findReversible(i) {
+    let reversible = [];
+    [
+      [-1, -1],
+      [-1, 0],
+      [-1, 1],
+      [0, 1],
+      [0, -1],
+      [1, 1],
+      [1, 0],
+      [1, -1]
+    ].forEach(([nr, nc]) => {
+      const isRange = (i) => (0 <= i && i < 8);
+
+      let[r, c] = calcRC(i);
+      let [nextR, nextC] = [r + nr, c + nc];
+      let reversibleInLine = [calcIdx(nextR, nextC)];
+      if (!isRange(nextR) || !isRange(nextC) ||
+         this.state.values[calcIdx(nextR, nextC)] === 0 || 
+         this.state.values[calcIdx(nextR, nextC)] === (this.state.blackIsNext ? 1 : -1)){
+        return;
+      }
+
+      nextR += nr;
+      nextC += nc;
+      while (isRange(nextR) && isRange(nextC)) {
+        if(this.state.values[calcIdx(nextR, nextC)] === (this.state.blackIsNext ? 1 : -1)){
+          reversible = reversible.concat(reversibleInLine);
+          return;
+        }
+        reversibleInLine.push(calcIdx(nextR, nextC));
+        nextR += nr;
+        nextC += nc;
+      }
+    });
+
+    return reversible;
   }
 
   render() {
@@ -38,6 +87,10 @@ class Game extends React.Component {
 
 function calcIdx(row, col) {
   return 8 * row + col;
+}
+
+function calcRC(idx){
+  return [Math.floor(idx / 8), idx % 8];
 }
 
 export default Game;
