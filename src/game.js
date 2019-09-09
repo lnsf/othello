438361fd58df6b65674ba5884fc073ec0be93418
lnsf <lnsf03@gmail.com>
2019-09-09 10:38:07 +0900

    :tada: Display turn and number of stones

--- src/game.js
+++ src/game.js
@@ -7,7 +7,9 @@ class Game extends React.Component {
 
     this.state = {
       values: Array(64).fill(0),
-      blackIsNext: true
+      blackIsNext: true,
+      black: 2,
+      white: 2
     };
 
     this.state.values[calcIdx(3, 3)] = 1;
@@ -25,12 +27,15 @@ class Game extends React.Component {
 
     let next = this.state;
     next.values[i] = this.state.blackIsNext ? 1 : -1;
-
     reversible.forEach((r) => {
       next.values[r] = this.state.blackIsNext ? 1 : -1;
     });
 
     next.blackIsNext = !this.state.blackIsNext;
+
+    next.black = this.count(1);
+    next.white = this.count(-1);
+
     this.setState(next);
   }
 
@@ -73,9 +78,31 @@ class Game extends React.Component {
     return reversible;
   }
 
+  count(s) {
+    const sorted = this.state.values.slice().sort();
+    const first = sorted.indexOf(s);
+    const last = sorted.lastIndexOf(s);
+    if (first === -1) {
+      return 0;
+    }
+    return last - first + 1;
+  }
+
   render() {
     return (
       <div>
+        <div className="horizontal">
+          <div class="status-inner" id="status-black">
+            <h2>Black</h2>
+            <h3>{this.state.black}</h3>
+            <h3>{this.state.blackIsNext ? "TURN" : ""}</h3>
+          </div>
+          <div class="status-inner" id="status-white">
+            <h2>White</h2>
+            <h3>{this.state.white}</h3>
+            <h3>{this.state.blackIsNext ? "" : "TURN"}</h3>
+          </div>
+        </div>
         <Board
           values={this.state.values}
           onClick={(i) => this.handleClick(i)}
